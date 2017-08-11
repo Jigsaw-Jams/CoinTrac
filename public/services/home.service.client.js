e3debaf7dfaec4f9bc1441e29ad2c01761a508b1
dj <djstras963@gmail.com>
2017-08-11 19:12:58 -0400

    Migrated search feature to home page, and display price of top 12 currencies

--- public/services/home.service.client.js
+++ public/services/home.service.client.js
@@ -1,124 +1,92 @@
 (function() {
     angular
-        .module("CoinTrak")
+        .module("CoinTrac")
         .factory("HomeService", HomeService);
 
     function HomeService($http) {
-        var coins = [
-            {
-                "id": "bitcoin", 
-                "name": "Bitcoin", 
-                "symbol": "BTC", 
-                "price_usd": "2975.25", 
-                "available_supply": "16488925.0"
-            }, 
-            {
-                "id": "ethereum", 
-                "name": "Ethereum", 
-                "symbol": "ETH", 
-                "price_usd": "224.249", 
-                "available_supply": "93787003.0", 
-            }, 
-            {
-                "id": "ripple", 
-                "name": "Ripple", 
-                "symbol": "XRP", 
-                "price_usd": "0.174175", 
-                "available_supply": "38343841883.0", 
-            }, 
-            {
-                "id": "bitcoin-cash", 
-                "name": "Bitcoin Cash", 
-                "symbol": "BCH", 
-                "price_usd": "217.568", 
-                "available_supply": "16482875.0", 
-            }, 
-            {
-                "id": "litecoin", 
-                "name": "Litecoin", 
-                "symbol": "LTC", 
-                "price_usd": "45.752", 
-                "available_supply": "52319507.0"
-            }, 
-            {
-                "id": "nem", 
-                "name": "NEM", 
-                "symbol": "XEM", 
-                "price_usd": "0.222314", 
-                "available_supply": "8999999999.0"
-            }, 
-            {
-                "id": "ethereum-classic", 
-                "name": "Ethereum Classic", 
-                "symbol": "ETC", 
-                "price_usd": "15.1048", 
-                "available_supply": "94226300.0"
-            }, 
-            {
-                "id": "dash",
-                "name": "Dash",
-                "symbol": "DASH",
-                "price_usd": "187.172",
-                "available_supply": "7472727.0"
-            }, 
-            {
-                "id": "iota", 
-                "name": "IOTA", 
-                "symbol": "MIOTA", 
-                "price_usd": "0.380045",
-                "available_supply": "2779530283.0"
-            }, 
-            {
-                "id": "monero", 
-                "name": "Monero", 
-                "symbol": "XMR", 
-                "price_usd": "45.4643", 
-                "available_supply": "14886077.0",
-            }, 
-        ];
-
         var api = {
-            "getBitcoin" : getBitcoin,
-            "getCoinById": getCoinById,
-            "getCoinsBySupply": getCoinsBySupply
+            "getCoins" : getCoins,
+            "getSearchSource": getSearchSource
         };
+
         return api;
 
-        function getBitcoin() {
-            var url = "https://api.coinmarketcap.com/v1/ticker/bitcoin/";
-            return $http.get(url);
+
+        function getCoins(limit) {
+            var url = `https://api.coinmarketcap.com/v1/ticker/?limit=${limit}`;
+            return $http.get(url)
+                .then(function (response) {
+                    var coins = response.data;
+                    return coins;
+                });
+
         }
 
-        /**
-         * Get the coin with the specificed @coinId
-         * 
-         * @param {*} coinId 
-         */
-        function getCoinById(coinId) {
-            for (var c in coins) {
-                if(coins[c].id === coinId) {
-                    return coins[c];
-                }
-            }
+        function getSearchSource() {
+            var url = "https://api.coinmarketcap.com/v1/ticker/";
+            return $http.get(url)
+                .then(function (response) {
+                    var coins = response.data;
+                    var searchSource = [];
+
+                    for (coin in coins) {
+                        currentCoin = coins[coin];
+                        var id = currentCoin['id'];
+                        var symbol = currentCoin['symbol'].toUpperCase();
+
+                        var searchResult = {
+                            'id'   : id,
+                            'label': `${id} [${symbol}]`
+                        }
+
+                        searchSource.push(searchResult);
+                    }
+
+                    return searchSource;
+                });
         }
+        // function getBitcoin() {
+        //     var url = "https://api.coinmarketcap.com/v1/ticker/bitcoin/";
+        //     return $http.get(url);
+        // }
+
+        // var api = {
+        //     "getBitcoin" : getBitcoin,
+        //     "getCoinById": getCoinById,
+        //     "getCoinsBySupply": getCoinsBySupply
+        // };
+        // return api;
+
+        // /**
+        //  * Get the coin with the specificed @coinId
+        //  * 
+        //  * @param {*} coinId 
+        //  */
+        // function getCoinById(coinId) {
+        //     for (var c in coins) {
+        //         if(coins[c].id === coinId) {
+        //             return coins[c];
+        //         }
+        //     }
+        // }
 
 
-        /**
-         * Get all coins with a supply above @supply
-         * 
-         * @param {number} supply 
-         */
-        function getCoinsBySupply(supply) {
-            var result = [];
+        // /**
+        //  * Get all coins with a supply above @supply
+        //  * 
+        //  * @param {number} supply 
+        //  */
+        // function getCoinsBySupply(supply) {
+        //     var result = [];
 
-            for (var c in coins) {
-                if(coins[c].available_supply >= supply) {
-                    result.push(coins[c]);
-                }
-            }
+        //     for (var c in coins) {
+        //         if(coins[c].available_supply >= supply) {
+        //             result.push(coins[c]);
+        //         }
+        //     }
 
-            return result; 
-        }
+        //     return result; 
+        // }
 
     }
 })();
--- public/views/search/controllers/search.controller.client.js
+++ public/views/search/controllers/search.controller.client.js
@@ -1,6 +1,6 @@
 (function() {
     angular
-        .module("CoinTrak")
+        .module("CoinTrac")
         .controller("SearchController", SearchController);
 
         function SearchController(SearchService) {
@@ -8,11 +8,8 @@
             model.searchForCoin = searchForCoin;
 
             function init() {
-                // model.coinids = SearchService.getAllCoinIds();
-                // console.log(model.coinids);
                 SearchService.getAllCoinIds()
                     .then(function (coinIds) {
-                        console.log(coinIds);
                         model.coinIds = coinIds;
                     })
             }
@@ -21,7 +18,6 @@
 
             // find coins with an ID like 'coinId' set model.searchResult as that new array
             function searchForCoin(coinId) {
-                console.log('yoooooo');
                 // https://stackoverflow.com/questions/5324798/how-to-search-an-array-in-jquery-like-sql-like-value-statement
                 var coinIdUppercase = coinId.toUpperCase();
                 var result = $.grep(model.coinIds, function(value) {
