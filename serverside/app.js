f6974016859557722b2fdba77fd9cce80e6adfea
dj <djstras963@gmail.com>
2017-08-11 23:59:42 -0400

    typo

--- serverside/app.js
+++ serverside/app.js
@@ -1,6 +1,6 @@
 // Build connection string
 var connectionString = 'mongodb://127.0.0.1:27017/cointrac'; // for local connection
-if(process.env.=MONGODB_URI) {                       // check if running remotely
+if(process.env.MONGODB_URI) {                       // check if running remotely
     // var username = process.env.MLAB_USERNAME_WEBDEV;         // get from environment
     // var password = process.env.MLAB_PASSWORD_WEBDEV;
     // connectionString = 'mongodb://' + username + ':' + password;
