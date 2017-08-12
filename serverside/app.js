91c59d50a5ddf33ede2b6df182ded33209e6457e
dj <djstras963@gmail.com>
2017-08-11 23:58:15 -0400

    new mlab

--- serverside/app.js
+++ serverside/app.js
@@ -1,10 +1,11 @@
 // Build connection string
 var connectionString = 'mongodb://127.0.0.1:27017/cointrac'; // for local connection
-if(process.env.MLAB_USERNAME_WEBDEV) {                       // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV;         // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
-    connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds139288.mlab.com:39288/heroku_h69gv6xf';
+if(process.env.=MONGODB_URI) {                       // check if running remotely
+    // var username = process.env.MLAB_USERNAME_WEBDEV;         // get from environment
+    // var password = process.env.MLAB_PASSWORD_WEBDEV;
+    // connectionString = 'mongodb://' + username + ':' + password;
+    // connectionString += '@ds139288.mlab.com:39288/heroku_h69gv6xf';
+    var connectionString = process.env.MONGODB_URI;
 }
 
 // require dependencies and connect to database
