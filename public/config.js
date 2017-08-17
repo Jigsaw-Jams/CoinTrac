75983a54276ed14621363ee446674f6f2d75094d
dj <djstras963@gmail.com>
2017-08-17 19:07:27 -0400

    Removing more stuff from search.

--- public/config.js
+++ public/config.js
@@ -14,12 +14,6 @@
                     currentUser: getCurrentUser
                 }
             })
-            // -------- Search --------
-            .when("/search", {
-                templateUrl : "views/search/templates/search.view.client.html",
-                controller: "SearchController",
-                controllerAs: "model"
-            })
             // -------- Details --------
             .when("/details/:coinId", {
                 templateUrl : "views/details/templates/details.view.client.html",
